0cb45ef62f8a35936a9e9dceafbc29e1addc9172
Dmytro <d.rossoshnyh@gmail.com>
2025-12-10 20:11:30 +0200

    Update portfolio

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ import ProjectCard from './ProjectCard'
 export default function Projects() {
   return (
     <section id="projects" className="flex flex-col gap-6">
-      <h2 className="text-2xl font-bold">Проекты</h2>
+      <h2 className="text-2xl font-bold">Prosjekter</h2>
       <div className="grid md:grid-cols-2 gap-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
